234b22c6c90172d65c366da7fd27bad20da8768c
FernandoAbitia <47840971+FernandoAbitia@users.noreply.github.com>
2019-02-24 10:49:44 -0700

    Actualización

--- Proveedores/Proveedores/ManejaFacturas.cs
+++ Proveedores/Proveedores/ManejaFacturas.cs
@@ -8,13 +8,44 @@ namespace Proveedores
 {
     class ManejaFacturas
     {
-        private Dictionary<int, Factura> facturas;
-        private List<DetalleFactura> DetalleFactura;
+        private Dictionary<int, Factura> Facturas;
 
         public ManejaFacturas()
         {
-            facturas = new Dictionary<int, Factura>();
-            DetalleFactura = new List<DetalleFactura>();
+            Facturas = new Dictionary<int, Factura>();
+        }
+        public void AgregarFactura(int Clave, int ClaveProv, float Importe, string Dia, string Mes, string Año)
+        {
+            Facturas.Add(Clave, new Factura(Clave, ClaveProv, Importe, Dia, Mes, Año));
+        }
+        public int BuscaFactura(int Clave)
+        {
+            for (int i = 0; i < Facturas.Count; i++)
+            {
+                if (Facturas[i].pClave == Clave)
+                    return i;
+            }
+            return -1;
+        }
+        public Factura RetornaFactura(int Clave)
+        {
+            for (int i = 0; i < Facturas.Count; i++)
+            {
+                if (Facturas[i].pClave == Clave)
+                    return Facturas[i];
+            }
+            return null;
+        }
+        public string ImprimeIndividual(int Clave)
+        {
+            return RetornaFactura(Clave).ToString();
+        }
+        public string ImprimeTodo()
+        {
+            string msj="";
+            for(int i = 0; i < Facturas.Count; i++)
+                msj += Facturas[i].ToString();
+            return msj;
         }
     }
 }
