ff34c6fff77faac265f026c3700ed441590abe13
FernandoAbitia <47840971+FernandoAbitia@users.noreply.github.com>
2019-02-25 22:47:09 -0700

    UPDATE

--- Proveedores/Proveedores/CapaNegocioFactura.cs
+++ Proveedores/Proveedores/CapaNegocioFactura.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Proveedores
 {
@@ -23,48 +24,41 @@ namespace Proveedores
 
         public void AgregaFactura()
         {
-
-            Console.WriteLine("***AGREGANDO FACTURA***");
+            Console.WriteLine("\n***AGREGANDO FACTURA***");
             Console.WriteLine("PROPORCIONE EL NÚMERO DE LA FACTURA: ");
             int ClaveFact = Leer.Int();
-            while(!ValidaValor(ClaveFact) || mF.BuscaFactura(ClaveFact) ==-1)
+            while(mF.BuscaFacturaClave(ClaveFact) !=-1 || !ValidaValor(ClaveFact))
             {
                 Console.WriteLine("NÚMERO DE FACTURA NO ACEPTADO, PROPORCIONE OTRO");
                 ClaveFact = Leer.Int();
             }
             Console.WriteLine("PROPORCIONE EL NÚMERO DEL PROVEEDOR: ");
             int ClaveProv = Leer.Int();
-            if (!ValidaValor(ClaveProv) ||mP.getPosClave(ClaveFact) ==-1)
+            while (mP.getPosClave(ClaveProv) ==-1 || !ValidaValor(ClaveProv))
             {
                 Console.WriteLine("NÚMERO DE PROVEEDOR NO ACEPTADO, PROPORCIONE OTRO");
-                ClaveProv=Leer.Int();
+                ClaveProv = Leer.Int();
             }
-            Console.WriteLine("PROPORCIONE EL IMPORTE");//<---- SE PIDE IMPORTE
-            float Importe = Leer.Float();
-            while (!ValidaValor(Importe))
-            {
-                Console.WriteLine("PROPORCIONE UN IMPORTE VÁLIDO");
-                Importe = Leer.Int();
-            }
+            Console.WriteLine("PROVEEDOR SELECCIONADO: {0}\n",mP.RetornaProveedorClave(ClaveProv).ToString());
             Console.WriteLine("PRPOPORCIONE LA FECHA DE LA FACTURA EN FORMATO dd/mm/yyyy");
             string Fecha = Leer.String();
             Fecha = Fecha.Trim();
             DateTime dt;
-            while (!DateTime.TryParseExact(Fecha, "dd/MM/yyyy", null, DateTimeStyles.None, out dt) && !ValidaAño(Fecha.Substring(6, 4)))
+            while (!DateTime.TryParseExact(Fecha, "dd/MM/yyyy", null, DateTimeStyles.None, out dt) && !mF.ValidaNula(Fecha))
             {
                 Console.WriteLine("LA FECHA NO ES VÁLIDA, PROPORCIONE OTRA");
                 Fecha = Leer.String();
             }
 
-            mF.AgregarFactura(ClaveFact, ClaveProv, Importe, Fecha.Substring(0, 2), Fecha.Substring(3, 2), Fecha.Substring(6, 4));
-            Console.WriteLine(mF.ImprimeIndividual(ClaveFact));
+            mF.AgregarFactura(ClaveFact, ClaveProv, Fecha.Substring(0, 2), mF.ConvierteMes(Fecha.Substring(3,2)), Fecha.Substring(6, 4));
+            Console.WriteLine("Factura creada: {0}\n",mF.ImprimeIndividual(ClaveFact));
             CapturaArticulos(ClaveFact, ClaveProv);
         }
         public void CapturaArticulos(int ClaveFactura,int ClaveProv)
         {
             Console.WriteLine("***AGREGANDO ARTÍCULOS A LA FACTURA***");
-            int TotalArt, CantArt;
-            float Imp = 0, TotalImp = 0;
+            int CantArt;
+            float TotalArt,Precio, TotalImp = 0;
             Console.Write("¿CUÁNTOS ARTÍCULOS DESEA AGREGAR EN TOTAL?: ");
             TotalArt = Leer.Int();
             while (TotalArt<1 || TotalArt > 3)
@@ -82,28 +76,39 @@ namespace Proveedores
                     Console.WriteLine("EL ARTÍCULO NO EXISTE, PROPORCIONE OTRA CLAVE");
                     ClaveArt = Leer.Int();
                 }
+                Console.WriteLine("ARTICULO SELECCIONADO: {0}\n", mA.RetornaArticulo(ClaveArt).ToString());
                 Console.WriteLine("INTRODUZCA LA CANTIDAD DE DICHO ARTÍCULO");
                 CantArt = Leer.Int();
-                while (!ValidaValor(CantArt) || CantArt>3)
+                while (!ValidaValor(CantArt) || CantArt>3 || TotalArt-CantArt<0)
                 {
                     Console.WriteLine("CANTIDAD NO ACEPTADA, PROPORCIONE OTRA CANTIDAD");
                     CantArt = Leer.Int();
                 }
                 TotalArt -= CantArt;
-                Imp = CantArt * mA.RetornaArticulo(ClaveArt).pPrecio;//<-----SE GENERA OTRO IMPORTE¿?
-                TotalImp += Imp;
-                mD.AgregarDetalle(ClaveFactura, ClaveArt, CantArt, Imp);
+                Precio = mA.RetornaArticulo(ClaveArt).pPrecio;
+                TotalImp += Precio*CantArt;
+                mD.AgregarDetalle(ClaveFactura, ClaveArt,CantArt, Precio);
             }
-            mP.ObtieneProveedorClave(ClaveProv).pSaldo += TotalImp;
-            //ACTUALIZAR IMPORTE A LA FACTURA¿?
-
+            Console.WriteLine("\n***MOSTRANDO DETALLES AGREGADOS***\n{0}", mD.ImprimePorClave(ClaveFactura));
+            Factura F = mF.RetornaFactura(ClaveFactura);
+            F.pImporte = TotalImp;
+            Proveedor P = mP.RetornaProveedorClave(ClaveProv);
+            P.pSaldo += TotalImp;
+            Console.WriteLine("Importe para la factura {0}",F.pImporte);
+            Console.WriteLine("Saldo del proveedor: {0}",P.pSaldo);
         }
-        public bool ValidaAño(string Año)
+        public void ConsultaFacturas()
         {
-            int A = int.Parse(Año);
-            if (A > 2019)
-                return false;
-            return true;
+            Console.WriteLine("\n***MOSTRANDO FACTURAS POR PROVEEDOR***");
+            Console.WriteLine("PROPORCIONE EL NOMBRE DEL PROVEEDOR: ");
+            string Nombre = Console.ReadLine();
+            int Proveedor;
+            Proveedor = mP.BuscarPosNombre(Nombre.ToUpper());
+            Console.WriteLine(mF.ImprimeFacturaClaveProv(Proveedor,mD,mA));
+        }
+        public void ImprimirFacturas()
+        {
+            Console.WriteLine(mF.ImprimeTodo());
         }
         public bool ValidaValor(float Valor)
         {
